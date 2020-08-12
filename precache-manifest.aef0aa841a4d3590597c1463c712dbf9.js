2eca3641347111bc0715c693ca55fd52538b2cfe
alex9692 <battle253@gmail.com>
2020-08-12 13:12:18 +0530

    Updates

---
+++ precache-manifest.aef0aa841a4d3590597c1463c712dbf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5c332e784138fd8bd971b1b08dae8c",
+    "url": "/monster_rolodex-react/index.html"
+  },
+  {
+    "revision": "bb41e2407d481f5f946f",
+    "url": "/monster_rolodex-react/static/css/main.15c43277.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster_rolodex-react/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster_rolodex-react/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb41e2407d481f5f946f",
+    "url": "/monster_rolodex-react/static/js/main.52ed5de1.chunk.js"
+  },
+  {
+    "revision": "2cad87042b387c5e98fe",
+    "url": "/monster_rolodex-react/static/js/runtime-main.63bc2937.js"
+  }
+]);
